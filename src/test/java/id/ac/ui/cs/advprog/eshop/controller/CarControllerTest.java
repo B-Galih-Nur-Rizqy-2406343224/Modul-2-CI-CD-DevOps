dc1231cf496630c5c06ad5fd7a9b07abcdefdc0e
galihnur9 <galihnurr9@gmail.com>
2026-02-27 02:30:26 +0700

    refactor: Change CarController class from package-private to public access modifier

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/CarControllerTest.java
@@ -0,0 +1,106 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import id.ac.ui.cs.advprog.eshop.model.Car;
+import id.ac.ui.cs.advprog.eshop.service.CarService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(CarController.class)
+class CarControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockitoBean
+    CarService carService;
+
+    @Test
+    void testCreateCarPage() throws Exception {
+        mockMvc.perform(get("/car/createCar"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("createCar"))
+                .andExpect(model().attributeExists("car"));
+    }
+
+    @Test
+    void testCreateCarPost() throws Exception {
+        mockMvc.perform(post("/car/createCar")
+                        .param("carName", "Avanza")
+                        .param("carColor", "Red")
+                        .param("carQuantity", "10"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("listCar"));
+
+        verify(carService, times(1)).create(any(Car.class));
+    }
+
+    @Test
+    void testCarListPage() throws Exception {
+        Car car = new Car();
+        car.setCarId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        car.setCarName("Avanza");
+        car.setCarColor("Red");
+        car.setCarQuantity(10);
+
+        when(carService.findAll()).thenReturn(List.of(car));
+
+        mockMvc.perform(get("/car/listCar"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("carList"))
+                .andExpect(model().attributeExists("cars"));
+
+        verify(carService, times(1)).findAll();
+    }
+
+    @Test
+    void testEditCarPage() throws Exception {
+        Car car = new Car();
+        car.setCarId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        car.setCarName("Avanza");
+        car.setCarColor("Red");
+        car.setCarQuantity(10);
+
+        when(carService.findById("eb558e9f-1c39-460e-8860-71af6af63bd6")).thenReturn(car);
+
+        mockMvc.perform(get("/car/editCar/eb558e9f-1c39-460e-8860-71af6af63bd6"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("editCar"))
+                .andExpect(model().attributeExists("car"));
+
+        verify(carService, times(1)).findById("eb558e9f-1c39-460e-8860-71af6af63bd6");
+    }
+
+    @Test
+    void testEditCarPost() throws Exception {
+        mockMvc.perform(post("/car/editCar")
+                        .param("carId", "eb558e9f-1c39-460e-8860-71af6af63bd6")
+                        .param("carName", "Innova")
+                        .param("carColor", "Blue")
+                        .param("carQuantity", "5"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("listCar"));
+
+        verify(carService, times(1)).update(eq("eb558e9f-1c39-460e-8860-71af6af63bd6"), any(Car.class));
+    }
+
+    @Test
+    void testDeleteCar() throws Exception {
+        mockMvc.perform(post("/car/deleteCar")
+                        .param("carId", "eb558e9f-1c39-460e-8860-71af6af63bd6"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("listCar"));
+
+        verify(carService, times(1)).deleteCarById("eb558e9f-1c39-460e-8860-71af6af63bd6");
+    }
+}
